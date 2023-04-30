70ddf80c7c5b1ea3c865fcfd267c3506bcbbde5b
RIDWAN AIMAR <ridwanaimar3@hotmail.com>
2023-05-01 02:06:44 +0800

    Update 2

---
+++ task3Minefield/Minefield.cs
@@ -0,0 +1,114 @@
+﻿namespace Minefield
+{
+  public enum Field { Bomb, Empty, Safe }
+
+  //Minefield Method
+  public class Minefield
+  {
+    private Field[][] minefield;
+
+    public Minefield(Field[][] minefield) { this.minefield = minefield; }
+
+    // Method Display Minefield Interface
+    public void Display()
+    {
+      foreach (var fields in minefield)
+      {
+        foreach (var field in fields)
+        {
+          char symbol = 'E';
+          switch (field)
+          {
+            case Field.Safe:
+              Console.ForegroundColor = ConsoleColor.Green;
+              symbol = 'S';
+              break;
+            case Field.Bomb:
+              Console.ForegroundColor = ConsoleColor.Red;
+              symbol = 'B';
+              break;
+          }
+          Console.Write("[ ");
+          Console.Write("{0} ", symbol);
+          Console.Write("]");
+          Console.ResetColor();
+        }
+        Console.Write("\n");
+      }
+    }
+
+    //Mehtod Display Totoshka and Ally location
+    public void DisplayWithAlly()
+    {
+      List<Tuple<int, int>> coords = new List<Tuple<int, int>>();
+
+      for (int i = 0; i < minefield.Length; i++)
+        for (int j = 0; j < minefield[i].Length; j++)
+          if (minefield[i][j] == Field.Safe)
+            coords.Add(new Tuple<int, int>(i, j));
+
+      Tuple<int, int>? tCoord = coords.Count > 0 ? coords[coords.Count - 1] : null;
+      Tuple<int, int>? aCoord = coords.Count > 1 ? coords[coords.Count - 2] : null;
+
+      Display();
+
+      if (tCoord != null) Console.WriteLine("Totoshka locatios: ({0}, {1})", tCoord.Item1, tCoord.Item2);
+      if (aCoord != null) Console.WriteLine("Ally location: ({0}, {1})", aCoord.Item1, aCoord.Item2);
+    }
+
+    public void Solve()
+    {
+      for (int i = 0; i < minefield.Length; i++)
+      {
+        bool found = false;
+        for (int j = 0; j < minefield[i].Length; j++)
+        {
+          // skip bomb/safe field or when found
+          if (found || minefield[i][j] == Field.Bomb || minefield[i][j] == Field.Safe) continue;
+
+          // find first line
+          if (i == 0)
+            if ((j - 1 > 0 && minefield[i + 1][j - 1] == Field.Empty) || (j + 1 < minefield.Length && minefield[i + 1][j + 1] == Field.Empty) || (minefield[i + 1][j] == Field.Empty))
+            {
+              minefield[i][j] = Field.Safe;
+              found = true;
+            }
+
+          // all next lines
+          if (CheckAdjacent(new Tuple<int, int>(i, j), minefield))
+          {
+            minefield[i][j] = Field.Safe;
+            found = true;
+            Console.WriteLine("Step {0}:", i + 1);
+            DisplayWithAlly();
+            Console.WriteLine();
+          }
+        }
+
+        // unable to find safe path
+        if (!found)
+          throw new Exception("There is no available safe path, Totoshka and Ally are TRAPPED");
+      }
+      Console.WriteLine("Totoshka had found the exit, Both Totoshka and Ally are FREE!");
+    }
+
+    private bool CheckAdjacent(Tuple<int, int> coord, Field[][] minefield)
+    {
+      var (r, c) = coord;
+      for (int i = r - 1; i <= r + 1; i++)
+      {
+        // if i is out of bounds
+        if (i < 0 || i >= minefield.Length) continue;
+
+        for (int j = c - 1; j <= c + 1; j++)
+        {
+          // if j is out of bounds
+          if (j < 0 || j >= minefield[i].Length) continue;
+
+          if (minefield[i][j] == Field.Safe) return true;
+        }
+      }
+      return false;
+    }
+  }
+}
--- task3Minefield/Program.cs
+++ task3Minefield/Program.cs
@@ -1,95 +1,61 @@
-﻿using System;
-
-public class Minefield
+﻿namespace Minefield
 {
-    private readonly int n;
-    private readonly int m;
-    private readonly bool[,] bombs;
-    private readonly int[,] currentPosition;
-
-    public Minefield(int n, int m)
+    class Program
     {
-        this.n = n;
-        this.m = m;
-        this.bombs = new bool[n, m];
-        this.currentPosition = new int[2, 2];
-    }
-
-    public void AddBomb(int x, int y)
-    {
-        bombs[x, y] = true;
-    }
-
-    public void ClearBomb(int x, int y)
-    {
-        bombs[x, y] = false;
-    }
-
-    public bool IsSafe(int x, int y)
-    {
-        return !bombs[x, y];
-    }
-
-    public void MoveToSafestPosition()
-    {
-        int x = currentPosition[0, 0];
-        int y = currentPosition[0, 1];
-        int bestX = x;
-        int bestY = y;
-        bool foundSafePosition = false;
-        for (int i = -1; i <= 1; i++)
+        //Main Method
+        static void Main(string[] args)
         {
-            for (int j = -1; j <= 1; j++)
+            Minefield mf = new Minefield(ReadInput());
+
+            try
             {
-                if (i == 0 && j == 0)
+                mf.Solve();
+            }
+            catch (Exception e)
+            {
+                mf.Display();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("\n{0}", e.Message);
+                Console.ResetColor();
+            }
+        }
+    
+        //Input Method
+        static Field[][] ReadInput()
+        {
+            List<Field[]> minefield = new List<Field[]>();
+
+            Console.WriteLine("Please enter minefield dimension nxm Eg.: 6 6");
+            string? line = Console.ReadLine();
+            string[] dimension = line!.Split(' ');
+            int n = Convert.ToInt32(dimension[0]);
+            int m = Convert.ToInt32(dimension[1]);
+            Console.WriteLine("Please fill up your minefield with 'B' for Bomb and 'E' for Empty. B B E B B B");
+      
+            while (n-- > 0)
+            {
+                List<Field> fields = new List<Field>();
+                line = Console.ReadLine();
+                foreach (var c in line!.Split(' '))
                 {
-                    continue;
-                }
-                int newX = x + i;
-                int newY = y + j;
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m)
-                {
-                    if (IsSafe(newX, newY))
+                    switch (c)
                     {
-                        bestX = newX;
-                        bestY = newY;
-                        foundSafePosition = true;
-                        break;
+                        case "E":
+                            fields.Add(Field.Empty);
+                            break;
+                        case "S":
+                            throw new Exception("You can only fill with 'Bomb' field and 'Empty' field");
+                        case "B":
+                            fields.Add(Field.Bomb);
+                            break;
+                        default:
+                            throw new Exception("You can only fill with 'B' field and 'E' field");
                     }
                 }
+                minefield.Add(fields.ToArray());
             }
-            if (foundSafePosition)
-            {
-                break;
-            }
-        }
-        currentPosition[0, 0] = bestX;
-        currentPosition[0, 1] = bestY;
-        currentPosition[1, 0] = bestX;
-        currentPosition[1, 1] = bestY;
-    }
 
-    public bool HasReachedEnd()
-    {
-        return currentPosition[0, 0] == n - 1 && currentPosition[0, 1] == m - 1;
-    }
-
-    public static void Main(string[] args)
-    {
-        int n = 10;
-        int m = 10;
-        Minefield minefield = new Minefield(n, m);
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                minefield.AddBomb(i, j);
-            }
+            return minefield.ToArray();
         }
-        while (!minefield.HasReachedEnd())
-        {
-            minefield.MoveToSafestPosition();
-        }
-        Console.WriteLine("Totoshka and Ally have successfully passed through the minefield!");
     }
 }
